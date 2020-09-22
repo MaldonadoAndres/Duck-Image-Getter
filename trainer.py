a9f017cf84d523825e0e54371f7ac6ed9c23ee71
Andres Maldonado <maldonadoandres327@gmail.com>
2020-09-21 20:23:14 -0500

    Training Method

---
+++ trainer.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+import utils as Utils
+import numpy as np
+
+
+X = pickle.load(open("X.pickle", "rb"))
+Y = pickle.load(open("Y.pickle", "rb"))
+Y = np.array(Y)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+model.compile(loss="categorical_crossentropy",
+              optimizer="adam", metrics=['accuracy'])
+
+model.fit(x=X, y=Y, batch_size=30, validation_split=0.1)
